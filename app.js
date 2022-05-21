db77504b950b3fe3f63f44237f795a7393d75abc
Isaque Menezes <isaque-menezes@hotmail.com>
2022-05-21 14:18:41 -0300

    03 js e css

--- app.js
+++ app.js
@@ -9,12 +9,27 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set("views", "./views");
 
-app.get('/:id?', function(req, res) {
+//Link de arquivos externos 1 opção
+//app.use("/css", express.static('css'));
+//app.use("/js", express.static('js'));
+//app.use("/images", express.static('img'));
+
+
+app.get('/', function(req, res) {
     //res.send("Essa é minha página inicial");
     //res.sendFile(__dirname+"/index.html"); 
     //res.render('index');
     //console.log(req.params.id);
-    res.render('index',{id:req.params.id});
+    res.render('index');
+});
+
+//Link por rotas
+app.get("/main",function(req,res){
+    res.sendFile(__dirname+'/js/main.js');
+});
+
+app.get("/style",function(req,res){
+    res.sendFile(__dirname+'/css/style.css');
 });
 
 app.listen(3000, function(req,res){
