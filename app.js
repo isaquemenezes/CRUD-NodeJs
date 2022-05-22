6217467a496bd011791015ede590069f4566cbdb
Isaque Menezes <isaque-menezes@hotmail.com>
2022-05-22 14:30:44 -0300

    port 03

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const sql=mysql.createPool({
 });
 
 //Port Pro heroku
-let port=process.env.PORT || 3000;
+const PORT=process.env.PORT || 3000;
 
 //sql.query("use nodejs");
 
@@ -137,8 +137,8 @@ app.post('/controllerRegister',urlEncodeParser,function(req,res){
     res.render('controllerRegister',{name:req.body.name});
 
 });
-
-*/
 app.listen(port, function(req,res){
     console.log('Servidor rodando')
 });
+*/
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
